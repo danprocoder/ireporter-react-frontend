aa4f2957b1726519fac81af867612eb1bc57a294
Daniel Austin <danielaustin718@gmail.com>
2019-03-29 10:00:30 +0100

    feat(homepage): Created homepage feature
    [Delivers #164009775]

---
+++ src/components/AdminNav.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+// import '../../assets/css/admin/nav.css';
+
+export default class extends Component {
+  render() {
+    return (
+      <div id="admin top">
+        <div class="sitename pull-left">
+          <a href="index.html">
+            <h2><i class="fa fa-bullhorn"></i> <span class="hide-responsive">iReporter</span></h2>
+          </a>
+        </div>
+        <a href="#" class="pull-left responsive-toggle"><i class="fa fa-bars"></i></a>
+        <ul class="navbar-menu pull-right">
+          <li class="dropdown">
+            <a href="#">{this.username} <i class="fa fa-caret-down"></i></a>
+            <div class="dropdown-menu">
+              <ul>
+                <li><a href="#" class="auth-logout">Logout</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+        <div class="clearfix"></div>
+      </div>
+    );
+  }
+}
---
+++ src/components/DefaultNav.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../../assets/css/nav.css';
+import '../../assets/css/default-nav.css';
+
+export default class extends Component {
+  render() {
+    return (
+      <div className="user red-bg">
+        <div id="navbar">
+          <div className="navbar-container">
+            <div className="navbar-inner">
+              <Link to="/" className="sitename"><i className="fa fa-bullhorn"></i> iReporter</Link>
+              <div className="navbar-menu horizontal">
+                <ul>
+                  <li><Link to="/login">Log In</Link></li>
+                  <li><Link to="/signup">Sign Up</Link></li>
+                </ul>
+              </div>
+              <button className="nav-toggle-responsive"><i className="fa fa-bars"></i></button>
+              <div className="clearfix"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
