dfdc4965c05696c3f49973509079b9c74bf7f837
Daniel Austin <danielaustin718@gmail.com>
2019-04-30 17:23:41 +0100

    feat(viewIncidents): create feature for users to view all the incidents they have created
    [Finishes #164010123]

--- src/components/user/UserNav.jsx
+++ src/components/user/UserNav.jsx
@@ -1,40 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { string as stringProp } from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../../../assets/scss/user/user-nav.scss';
 
-const UserNav = ({ username }) => (
-  <div className="user-nav red-bg">
-    <div id="navbar">
-      <div className="container clearfix">
-        <Link to="/dashboard" className="sitename f-left">
-          <i className="fa fa-bullhorn" />
-          {' '}
-          iReporter
-        </Link>
-        <div className="navbar-menu f-right">
-          <ul>
-            <li><Link to="/red-flags">Red Flags</Link></li>
-            <li><Link to="/interventions">Interventions</Link></li>
-            <li className="dropdown">
-              <a href="#">
-                {username}
-                {' '}
-                <i className="fa fa-angle-down" />
-              </a>
-              <ul className="dropdown-menu">
-                <li><Link to="/profile">My Profile</Link></li>
-                <li><a href="#" className="auth-logout">Log out</a></li>
+class UserNav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showUserDropdown: false,
+    };
+  }
+
+  render() {
+    const { username } = this.props;
+
+    const { showUserDropdown } = this.state;
+
+    return (
+      <div className="user-nav red-bg">
+        <div id="navbar">
+          <div className="container clearfix">
+            <Link to="/dashboard" className="sitename f-left">
+              <i className="fa fa-bullhorn" />
+              {' '}
+              iReporter
+            </Link>
+            <div className="navbar-menu f-right">
+              <ul>
+                <li><Link to="/red-flags">Red Flags</Link></li>
+                <li><Link to="/interventions">Interventions</Link></li>
+                <li className="dropdown">
+                  <a href="#">
+                    {username}
+                    {' '}
+                    <i className="fa fa-angle-down" />
+                  </a>
+                  {showUserDropdown && (
+                  <ul className="dropdown-menu">
+                    <li><Link to="/profile">My Profile</Link></li>
+                    <li><a href="#" className="auth-logout">Log out</a></li>
+                  </ul>
+                  )}
+                </li>
               </ul>
-            </li>
-          </ul>
+            </div>
+            <button className="nav-toggle-responsive f-right" type="button"><i className="fa fa-bars" /></button>
+          </div>
         </div>
-        <button className="nav-toggle-responsive f-right" type="button"><i className="fa fa-bars" /></button>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 UserNav.propTypes = {
   username: stringProp.isRequired,
